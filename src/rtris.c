ab60090d5b5b1b36ce64faebc9d55e193f54045d
OngakuKun <marcellehmkuhl@outlook.de>
2023-09-20 10:45:19 +0200

    Add Line Clearing

--- src/rtris.c
+++ src/rtris.c
@@ -7,16 +7,28 @@
 #include <stdio.h>
 #include <string.h>
 
+#define FIELD_WIDTH 12
+#define FIELD_HEIGHT 18
+
 const int screenWidth = WINDOW_WIDTH;
 const int screenHeight = WINDOW_HEIGHT;
 
-const int FIELD_WIDTH = 12;
-const int FIELD_HEIGHT = 18;
-
 const int ELEMENT_SIZE = 20;
+
+char fieldContent[FIELD_WIDTH][FIELD_HEIGHT] = {0};
+
+Vector2 currentBlockPos;
+bool currentBlock[4][4];
+int currentBlockType;
+int currentBlockRotation;
+
+bool isGameOver = false;
+
+RenderTexture2D fieldRender;
 
 enum ColorsT
 {
+	BLOCK_EMPTY,
 	BLOCK_I,
 	BLOCK_J,
 	BLOCK_L,
@@ -27,8 +39,9 @@ enum ColorsT
 };
 
 /** Colors for the Tetromino */
-const Color tColors[7] =
+const Color tColors[8] =
 {
+	{0, 0, 0, 255},
 	{0, 255, 255, 255},
 	{0, 0, 255, 255},
 	{255, 127, 0, 255},
@@ -47,7 +60,7 @@ void drawFieldBackground(Vector2 fPos)
 	DrawRectangleLines(fPos.x - 1, fPos.y - 1, FIELD_WIDTH * ELEMENT_SIZE + 2, FIELD_HEIGHT * ELEMENT_SIZE + 2, WHITE);
 }
 
-/** Draw one Field of a Tetromino 
+/** Draw one Field of a Tetromino
  *  @param posX X-Position of the Tetromino
  *  @param posY Y-Position of the Tetromino
  *  @param color Color of the Tetromino */
@@ -57,78 +70,117 @@ void drawElement(int posX, int posY, Color color)
 	DrawRectangleLines(posX, posY, ELEMENT_SIZE, ELEMENT_SIZE, WHITE);
 }
 
-void updateCurrentBlock(bool blocks[4][4], int rotation, int type)
+void updateCurrentBlock(int rotation, int type)
 {
 	switch (type)
 	{
 		case BLOCK_I:
-			memcpy(blocks[0], &blockI[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockI[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockI[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockI[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockI[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockI[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockI[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockI[rotation][12], 4 * sizeof(bool));
 			break;
 		case BLOCK_J:
-			memcpy(blocks[0], &blockJ[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockJ[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockJ[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockJ[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockJ[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockJ[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockJ[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockJ[rotation][12], 4 * sizeof(bool));
 			break;
 		case BLOCK_L:
-			memcpy(blocks[0], &blockL[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockL[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockL[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockL[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockL[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockL[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockL[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockL[rotation][12], 4 * sizeof(bool));
 			break;
 		case BLOCK_O:
-			memcpy(blocks[0], &blockO[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockO[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockO[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockO[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockO[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockO[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockO[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockO[rotation][12], 4 * sizeof(bool));
 			break;
 		case BLOCK_S:
-			memcpy(blocks[0], &blockS[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockS[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockS[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockS[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockS[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockS[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockS[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockS[rotation][12], 4 * sizeof(bool));
 			break;
 		case BLOCK_T:
-			memcpy(blocks[0], &blockT[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockT[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockT[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockT[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockT[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockT[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockT[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockT[rotation][12], 4 * sizeof(bool));
 			break;
 		case BLOCK_Z:
-			memcpy(blocks[0], &blockZ[rotation][0], 4 * sizeof(bool));
-			memcpy(blocks[1], &blockZ[rotation][4], 4 * sizeof(bool));
-			memcpy(blocks[2], &blockZ[rotation][8], 4 * sizeof(bool));
-			memcpy(blocks[3], &blockZ[rotation][12], 4 * sizeof(bool));
+			memcpy(currentBlock[0], &blockZ[rotation][0], 4 * sizeof(bool));
+			memcpy(currentBlock[1], &blockZ[rotation][4], 4 * sizeof(bool));
+			memcpy(currentBlock[2], &blockZ[rotation][8], 4 * sizeof(bool));
+			memcpy(currentBlock[3], &blockZ[rotation][12], 4 * sizeof(bool));
 			break;
 	}
 }
 
 /** @param currentBlock Current Tetromino
  *  @param field Field Array
- *  @param pPos Tetromino Position to Move 
+ *  @param pPos Tetromino Position to Move
  *  @returns TRUE if no Collisions accoures */
-bool DoesPieceFit(bool currentBlock[4][4], char field[FIELD_WIDTH][FIELD_HEIGHT], Vector2 pPos)
+bool DoesPieceFit(Vector2 pPos)
 {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			if (!currentBlock[i][j])
 				continue;
-			
+
 			// X,Y Position in Field
 			int posX = pPos.x + j;
 			int posY = pPos.y + i;
 
-			if (posX < 0 || posX > FIELD_WIDTH - 1 || posY > FIELD_HEIGHT - 1)
-				return false;
-
-			if (field[posX][posY] != 0)
+			if (posX < 0 || posX >= FIELD_WIDTH || posY >= FIELD_HEIGHT || fieldContent[posX][posY] != 0)
 				return false;
 		}
 	}
 	return true;
+}
+
+void clearLines()
+{
+	for (int i = FIELD_HEIGHT - 1; i >= 0; i--) {
+		bool isFull = true;
+		TraceLog(LOG_TRACE, "Row %d isFull = %d", i, isFull);
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			if (fieldContent[j][i] == 0) {
+				isFull = false;
+				TraceLog(LOG_TRACE, "Row %d Colum %d isFull = %d", j, i, isFull);
+				break;
+			}
+		}
+
+		if (isFull) {
+			// Shift all lines above this line down
+			for (int k = i; k > 0; k--)
+			{
+				for (int j = 0; j < FIELD_WIDTH; j++)
+				{
+					fieldContent[j][k] = fieldContent[j][k - 1];
+				}
+			}
+
+			// Clear the top line
+			for (int j = 0; j < FIELD_WIDTH; j++)
+			{
+				fieldContent[j][0] = 0;
+			}
+		}
+	}
+}
+
+void spawnTetromino() {
+	currentBlockPos = (Vector2) {5, 0};
+	currentBlockType = GetRandomValue(BLOCK_I, BLOCK_Z);
+	currentBlockRotation = GetRandomValue(0, 3);
+
+	if (!DoesPieceFit(currentBlockPos)) {
+		isGameOver = true;
+	}
 }
 
 void appMain()
@@ -136,33 +188,31 @@ void appMain()
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(screenWidth, screenHeight, TextFormat("Raytris v%s", VERSION));
 
-	// SetExitKey(0);
-	SetTargetFPS(60);
-
+	SetExitKey(0);
 	SetTraceLogLevel(LOG_TRACE);
-
-	char fieldContent[FIELD_WIDTH][FIELD_HEIGHT];
-	Vector2 fieldPos = {GetScreenWidth()/2 - (FIELD_WIDTH*ELEMENT_SIZE)/2, GetScreenHeight()/2 - (FIELD_HEIGHT*ELEMENT_SIZE)/2};
+	SetRandomSeed(time(NULL));
+	SetTargetFPS(60);
 	
+	fieldRender = LoadRenderTexture(screenWidth, screenHeight);
+
+	spawnTetromino();
+	
+	Vector2 fieldPos = {280, 60};
+
 	// Init fieldContent to 0
 	for (int w = 0; w < FIELD_WIDTH; w++)
 		for (int h = 0; h < FIELD_HEIGHT; h++)
 			fieldContent[w][h] = 0;
-	
-	Vector2 currentBlockPos = { 5, 0 };
-	bool currentBlock[4][4];
-	int currentBlockType = GetRandomValue(0,6);
-	int currentBlockRotation = GetRandomValue(0,3);
-	updateCurrentBlock(currentBlock, currentBlockRotation, currentBlockType);
 
-	// Timing Variables
+	updateCurrentBlock(currentBlockRotation, currentBlockType);
 
+	double lastTick;
 
 	int LinesCleared = 0;
 	//--------------------------------------------------------------------------------------
 
 	// Main game loop
-	while (!WindowShouldClose())
+	while (!isGameOver)
 	{
 		// Update
 		//----------------------------------------------------------------------------------
@@ -171,10 +221,10 @@ void appMain()
 			currentBlockRotation++;
 			if (currentBlockRotation > 3)
 				currentBlockRotation = 0;
-			updateCurrentBlock(currentBlock, currentBlockRotation, currentBlockType);
-			if (!DoesPieceFit(currentBlock, fieldContent, currentBlockPos)) {
+			updateCurrentBlock( currentBlockRotation, currentBlockType);
+			if (!DoesPieceFit(currentBlockPos)) {
 				currentBlockRotation--;
-				updateCurrentBlock(currentBlock, currentBlockRotation, currentBlockType);
+				updateCurrentBlock(currentBlockRotation, currentBlockType);
 			}
 			if (currentBlockRotation < 0)
 				currentBlockRotation = 3;
@@ -184,10 +234,10 @@ void appMain()
 			currentBlockRotation--;
 			if (currentBlockRotation < 0)
 				currentBlockRotation = 3;
-			updateCurrentBlock(currentBlock, currentBlockRotation, currentBlockType);
-			if (!DoesPieceFit(currentBlock, fieldContent, currentBlockPos)) {
+			updateCurrentBlock(currentBlockRotation, currentBlockType);
+			if (!DoesPieceFit(currentBlockPos)) {
 				currentBlockRotation++;
-				updateCurrentBlock(currentBlock, currentBlockRotation, currentBlockType);
+				updateCurrentBlock(currentBlockRotation, currentBlockType);
 			}
 			if (currentBlockRotation > 3)
 				currentBlockRotation = 0;
@@ -195,79 +245,95 @@ void appMain()
 		else if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))
 		{ // LEFT MOVEMENT
 			currentBlockPos.x--;
-			if (!DoesPieceFit(currentBlock, fieldContent, currentBlockPos)) {
+			if (!DoesPieceFit(currentBlockPos)) {
 				currentBlockPos.x++;
 			}
 		}
 		else if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT))
 		{ // RIGHT MOVEMENT
 			currentBlockPos.x++;
-			if (!DoesPieceFit(currentBlock, fieldContent, currentBlockPos))
+			if (!DoesPieceFit(currentBlockPos))
 				currentBlockPos.x--;
-		}else if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
+		}
+		else if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
 		{ // DOWN MOVEMENT
 			currentBlockPos.y++;
-			if (!DoesPieceFit(currentBlock, fieldContent, currentBlockPos))
+			if (!DoesPieceFit(currentBlockPos))
 				currentBlockPos.y--;
 		}
 
-		updateCurrentBlock(currentBlock, currentBlockRotation, currentBlockType);
+		updateCurrentBlock(currentBlockRotation, currentBlockType);
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (!currentBlock[i][j])
-					continue;
-				
-				// X,Y Position in Field
-				int posX = currentBlockPos.x + j;
-				int posY = currentBlockPos.y + i;
+		if (GetTime() >= lastTick + 2.0f) {
+			if (DoesPieceFit((Vector2) {currentBlockPos.x, currentBlockPos.y + 1})){
+				currentBlockPos.y++;
+			} 
+			else {
+				for (int i = 0; i < 4; i++) {
+					for (int j = 0; j < 4; j++) {
+						if (!currentBlock[i][j])
+							continue;
 
-				if (fieldContent[posX][posY] != 0)
-					return false;
+						// X,Y Position in Field
+						int posX = currentBlockPos.x + j;
+						int posY = currentBlockPos.y + i;
+
+						if (posX < 0 || posX >= FIELD_WIDTH || posY >= FIELD_HEIGHT || fieldContent[posX][posY] != 0)
+							break;
+
+						fieldContent[posX][posY] = currentBlockType;
+					}
+				}
+				clearLines();
+				spawnTetromino();
 			}
+			lastTick = GetTime();
+			if (!isGameOver)
+				updateCurrentBlock(currentBlockRotation, currentBlockType);
 		}
-		return true;
-
 		//----------------------------------------------------------------------------------
 
 		// Draw
 		//----------------------------------------------------------------------------------
-		BeginDrawing();
+		BeginTextureMode(fieldRender);
+			drawFieldBackground(fieldPos);
 
-		ClearBackground(BLACK);
-
-		drawFieldBackground(fieldPos);
-
-		for (int w = 0; w < FIELD_WIDTH; w++)
-		{
-			for (int h = 0; h < FIELD_HEIGHT; h++)
-			{
-				unsigned char c = fieldContent[w][h];
-				if (c != 0)
-					drawElement(fieldPos.x + (w * ELEMENT_SIZE), fieldPos.y + (h * ELEMENT_SIZE), tColors[c]);
-			}
-		}
-
-		// Draw Current Block
-		for (int w = 0; w < 4; w++) {
-			for (int h = 0; h < 4; h++) {
-				if (currentBlock[w][h]) {
-					drawElement(
-						fieldPos.x + (currentBlockPos.x + h) * ELEMENT_SIZE,
-						fieldPos.y + (currentBlockPos.y + w) * ELEMENT_SIZE,
-						tColors[currentBlockType]
-					);
+			for (int w = 0; w < FIELD_WIDTH; w++) {
+				for (int h = 0; h < FIELD_HEIGHT; h++) {
+					unsigned char c = fieldContent[w][h];
+					if (c != 0)
+						drawElement(fieldPos.x + (w * ELEMENT_SIZE), fieldPos.y + (h * ELEMENT_SIZE), tColors[c]);
 				}
 			}
-		}
 
-		DrawText(TextFormat("Rows Cleared: %d", LinesCleared), 10, 10, 18, RAYWHITE);
+			// Draw Current Block
+			for (int w = 0; w < 4; w++) {
+				for (int h = 0; h < 4; h++) {
+					if (currentBlock[w][h]) {
+						drawElement(
+							fieldPos.x + (currentBlockPos.x + h) * ELEMENT_SIZE,
+							fieldPos.y + (currentBlockPos.y + w) * ELEMENT_SIZE,
+							tColors[currentBlockType]);
+					}
+				}
+			}
+		EndTextureMode();
 
+		BeginDrawing();
+			ClearBackground(BLACK);
+			DrawRectangleGradientV(0, 0, GetScreenWidth(), GetScreenHeight(),
+			DARKBLUE, BLACK);
+
+			DrawTexturePro(fieldRender.texture, (Rectangle) {0, 0, screenWidth, -screenHeight}, (Rectangle) {GetScreenWidth()/2, GetScreenHeight()/2, GetScreenWidth(), GetScreenHeight()}, 
+			(Vector2) {GetScreenWidth()/2, GetScreenHeight()/2}, 0.0f, (Color) {255, 255, 255, 255});
+
+			DrawText(TextFormat("Rows Cleared: %d", LinesCleared), 10, 10, 18, RAYWHITE);
 		EndDrawing();
 		//----------------------------------------------------------------------------------
 	}
 
 	// De-Initialization
 	//--------------------------------------------------------------------------------------
+	UnloadRenderTexture(fieldRender);
 	CloseWindow();
 }

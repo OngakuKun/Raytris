40b3ee00f64f32530d8e9df0b17633b33357cc75
OngakuKun <MarcelLehmkuhl@outlook.de>
2023-09-23 22:26:28 +0200

    Add Window Creation

--- src/raytris.c
+++ src/raytris.c
@@ -1,8 +1,20 @@
 #include "defines.h"
+#include "raylib.h"
 
-int main()
-{
-	printf("Hello from main.c");
+int main() {
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, TextFormat("%s v%s", NAME, VERSION));
+
+	while(!WindowShouldClose()) {
+
+		BeginDrawing();
+
+			ClearBackground(BLACK);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
